1d5a7c348b3e35c82ee8f4ad86fb1200bd62e42f
asimaranov <a.simaranov@gmail.com>
2024-01-26 16:52:09 +0700

    Improvements

---
+++ app/components/TeamMemberCard.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+interface TeamMemberCardProps {
+    name: string;
+    description: string;
+    position: string;
+    image: string;
+    imageW: number;
+    imageH: number;
+    variant: 0 | 1 | 2
+}
+
+export const TeamMemberCard = (props: TeamMemberCardProps) => {
+    const color = props.variant == 0 ? 'text-left-accent' : props.variant == 1 ? 'text-middle-accent' : 'text-right-accent';
+
+    return (
+        <div className="mx-5 w-[33%] max-w-[400px] top-0 relative">
+            <div className={`absolute mx-5 h-full flex flex-col ${props.variant != 1 && 'top-[-100px]'}`}>
+                <div className="w-full flex flex-col items-center pb-5 justify-between">
+                    <Image src={props.image} width={props.imageW} height={props.imageH} alt="our_tech_upper" className="h-[200px] lg:h-[300px]" ></Image>
+                </div>
+                <div className="flex flex-col items-center ">
+                    <div className="flex justify-space-between justify-between">
+                        <div className="text-2xl font-bold">{props.name}</div>
+                    </div>
+                    <div className={`text-base font-normal max-w-[80%] py-2 ${color}`}>{props.position}</div>
+                    <div className="font-mono text-sm font-normal max-w-[80%]">{props.description}</div>
+                    <div className="flex-grow "></div>
+                </div>
+            </div>
+        </div>
+    )
+}
