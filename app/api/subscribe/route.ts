e2c6c72e45d931f0873ea7fe7ec15df72baaf125
asimaranov <a.simaranov@gmail.com>
2024-01-29 02:56:54 +0700

    Debug mongodb issue

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -6,17 +6,17 @@ export async function POST(request: Request) {
     const { email, termsChecked } = data;
 
     const client = await clientPromise;
-    const db = client.db(process.env.MONGODB_DB);
+    // const db = client.db(process.env.MONGODB_DB);
 
-    await db.collection("subscribe_emails").updateOne(
-        { email },
-        {
-          $set: {
-            termsChecked
-          },
-        },
-        { upsert: true },
-      )
+    // await db.collection("subscribe_emails").updateOne(
+    //     { email },
+    //     {
+    //       $set: {
+    //         termsChecked
+    //       },
+    //     },
+    //     { upsert: true },
+    //   )
 
     return Response.json({
         status: "ok"
