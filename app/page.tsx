36f5947d7604a1f8f9bed0f1fe0ce557e248d9bb
asimaranov <a.simaranov@gmail.com>
2024-02-06 15:36:08 +0700

    Fixed randzu links

--- app/page.tsx
+++ app/page.tsx
@@ -200,7 +200,7 @@ export default function Home() {
               exploreButton="/image/applications/play_button/play_right.svg"
               description="A tic tac toe like game. Two players take turns placing pieces on the board attempting to create lines of 5 of their own color. Game state is synced and proved using zkapp transactions"
               image="/image/applications/randzu.svg"
-              link="https://app.zknoid.io/ranzu/global"
+              link="https://app.zknoid.io/games/randzu/global"
             />
             <MoreGamesApplicationCard />
           </div>
