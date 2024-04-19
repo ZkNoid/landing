30427f15428e8e3506f5f02bc775b7987527a621
neogar <work.vatlecov@mail.ru>
2024-04-20 00:02:51 +0500

    feat: updated social links

--- app/components/MobileNavbar.tsx
+++ app/components/MobileNavbar.tsx
@@ -136,7 +136,7 @@ export const MobileNavbar = () => {
                         >
                             <div
                                 className={'w-3/5 h-screen bg-bg-dark flex flex-col gap-4 px-4 pb-4 pt-[80px] text-xl z-50'}>
-                                <Link href={'https://zknoid.medium.com/'} target="_blank" rel="noopener noreferrer">
+                                <Link href={'https://docs.zknoid.io/'} target="_blank" rel="noopener noreferrer">
                                     Docs
                                 </Link>
                                 <Link href={'https://zknoid.medium.com/'} target="_blank" rel="noopener noreferrer">
@@ -145,7 +145,7 @@ export const MobileNavbar = () => {
                                 <p>
                                     Social media:
                                 </p>
-                                <div className={'w-full flex flex-row gap-6'}>
+                                <div className={'w-full flex flex-row gap-2'}>
                                     {SOCIALS.map(social => (
                                         <Link key={social.name} className="cursor-pointer flex items-center justify-center"
                                             href={social.link} target="_blank" rel="noopener noreferrer">
--- app/constants/socials.ts
+++ app/constants/socials.ts
@@ -1,21 +1,39 @@
+import githubImg from '@/public/image/socials/github.svg'
+import twitterImg from '@/public/image/socials/twitter.svg'
+import mediumImg from '@/public/image/socials/medium.svg'
+import TelegramImage from '@/public/image/socials/telegram.svg'
+import DiscordImage from '@/public/image/socials/discrord.svg'
+
 export const SOCIALS = [
     {
         id: 'github',
         name: 'Github',
         link: 'https://github.com/ZkNoid',
-        image: '/image/socials/github.svg',
+        image: githubImg,
     },
     {
         id: 'twitter',
         name: 'Twitter',
         link: 'https://twitter.com/ZkNoid',
-        image: '/image/socials/twitter.svg',
+        image: twitterImg,
     },
     {
         id: 'medium',
         name: 'Medium',
         link: 'https://medium.com/@zknoid',
-        image: '/image/socials/medium.svg',
+        image: mediumImg,
+    },
+    {
+        id: 'telegram',
+        name: 'Telegram',
+        link: 'https://t.me/ZkNoid',
+        image: TelegramImage,
+    },
+    {
+        id: 'discord',
+        name: 'Discord',
+        link: 'https://discord.gg/hndRCZwQnb',
+        image: DiscordImage,
     },
 ];
 
