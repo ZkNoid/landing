a6193e966d366af70c2d667a3c70f8c9a3f03eef
Neo-Gar <nvatlecov@mail.ru>
2024-11-29 21:34:57 +0500

    Added mobile games carousel

--- sections/Games/Games.tsx
+++ sections/Games/Games.tsx
@@ -32,22 +32,26 @@ const Slide = ({
     <Link
       href={link}
       className={cn(
-        "rounded-[0.521vw] transition-transform min-w-0 flex-[0_0_25%] px-[0.781vw] pt-[0.781vw] pb-[0.521vw] flex flex-col",
+        "rounded-[2.353vw] lg:!rounded-[0.521vw] transition-transform min-w-0 flex-[0_0_85%] lg:!flex-[0_0_25%] px-[3.529vw] lg:!px-[0.781vw] pt-[3.529vw] lg:!pt-[0.781vw] pb-[2.353vw] lg:!pb-[0.521vw] flex flex-col",
         type === "currentSlide"
           ? "bg-white z-[2]"
           : type === "nextSlide"
-            ? "bg-gray-light scale-[0.85] z-[1]"
+            ? "bg-gray-light lg:!scale-[0.85] z-[1]"
             : "bg-black scale-[0.75]",
         className,
       )}
     >
       <div className={"flex flex-row items-center justify-between"}>
-        <div className={"flex flex-row gap-[0.521vw] items-center"}>
+        <div
+          className={
+            "flex flex-row gap-[2.353vw] lg:!gap-[0.521vw] items-center"
+          }
+        >
           {tags.map((tag, index) => (
             <span
               key={index}
               className={cn(
-                "rounded-full py-[0.156vw] border px-[0.313vw] text-[0.729vw] text-white font-helvetica-now leading-[110%]",
+                "rounded-full py-[0.824vw] lg:!py-[0.156vw] border px-[2vw] lg:!px-[0.313vw] text-[3.294vw] lg:!text-[0.729vw] text-white font-helvetica-now leading-[110%]",
                 type === "currentSlide"
                   ? "border-gray-light bg-gray-light"
                   : "border-white",
@@ -59,7 +63,7 @@ const Slide = ({
         </div>
         <span
           className={cn(
-            "rounded-full py-[0.156vw] border px-[0.313vw] text-[0.729vw] text-white font-helvetica-now leading-[110%]",
+            "rounded-full py-[0.824vw] lg:!py-[0.156vw] border px-[2vw] lg:!px-[0.313vw] text-[3.294vw] lg:!text-[0.729vw] text-white font-helvetica-now leading-[110%]",
             type === "currentSlide"
               ? "border-gray-light bg-gray-light"
               : "border-white",
@@ -70,18 +74,18 @@ const Slide = ({
       </div>
       <div
         className={
-          "mt-[0.781vw] rounded-[0.521vw] w-full h-[10.938vw] overflow-hidden"
+          "mt-[2.353vw] lg:!mt-[0.781vw] rounded-[2.353vw] lg:!rounded-[0.521vw] w-full h-[32vw] lg:!h-[10.938vw] overflow-hidden"
         }
       >
         <Image
           src={image}
           alt={"Slide image"}
-          className={"w-full h-full object-center object-contain"}
+          className={"w-full h-full object-center object-cover"}
         />
       </div>
       <span
         className={cn(
-          "mt-[0.521vw] font-helvetica-now font-medium text-[0.833vw]",
+          "mt-[2.353vw] lg:!mt-[0.521vw] font-helvetica-now font-medium text-[3.765vw] lg:!text-[0.833vw]",
           type === "currentSlide" ? "text-gray-dark" : "text-white",
         )}
       >
@@ -89,7 +93,7 @@ const Slide = ({
       </span>
       <span
         className={cn(
-          "mt-[0.26vw] font-helvetica-now text-[0.833vw]",
+          "mt-[1.176vw] lg:!mt-[0.26vw] font-helvetica-now text-[3.294vw] opacity-60 lg:!text-[0.833vw]",
           type === "currentSlide" ? "text-gray-dark" : "text-white",
         )}
       >
@@ -97,18 +101,18 @@ const Slide = ({
       </span>
       <div
         className={cn(
-          "mt-[0.781vw] h-px w-full",
+          "mt-[3.529vw] lg:!mt-[0.781vw] h-px w-full",
           type === "currentSlide" ? "bg-gray-dark" : "bg-white",
         )}
       />
       <div
         className={
-          "flex flex-row gap-[0.625vw] ml-auto items-center mt-[0.625vw] mr-px"
+          "flex flex-row gap-[1.176vw] lg:!gap-[0.625vw] ml-auto items-center mt-[2.353vw] lg:!mt-[0.625vw] mr-px"
         }
       >
         <span
           className={cn(
-            "text-[0.729vw] font-medium font-outfit  uppercase",
+            "text-[3.294vw] lg:!text-[0.729vw] font-medium font-outfit  uppercase",
             type === "currentSlide" ? "text-gray-light" : "text-white",
           )}
         >
@@ -121,7 +125,9 @@ const Slide = ({
             viewBox="0 0 27 27"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            className={"w-[1.406vw] h-[1.406vw]"}
+            className={
+              "w-[6.353vw] lg:!w-[1.406vw] h-[6.353vw] lg:!h-[1.406vw]"
+            }
           >
             <rect
               width="26.9122"
@@ -143,7 +149,9 @@ const Slide = ({
             viewBox="0 0 27 27"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            className={"w-[1.406vw] h-[1.406vw]"}
+            className={
+              "w-[6.353vw] lg:!w-[1.406vw] h-[6.353vw] lg:!h-[1.406vw]"
+            }
           >
             <rect
               width="26.8357"
@@ -237,37 +245,59 @@ export default function Games() {
     containScroll: false,
     slidesToScroll: 1,
     skipSnaps: true,
+    // loop: true,
+  });
+  const [mobileEmblaRef, mobileEmblaApi] = useEmblaCarousel({
+    startIndex: 0,
+    align: "start",
+    containScroll: false,
+    slidesToScroll: 1,
+    skipSnaps: true,
+    loop: true,
   });
   const [selectedIndex, setSelectedIndex] = useState<number>(0);
+  const [mobileSelectedIndex, setMobileSelectedIndex] = useState<number>(0);
 
   const onSelect = useCallback((emblaApi: any) => {
     setSelectedIndex(emblaApi.selectedScrollSnap());
   }, []);
+  const onSelectMobile = useCallback((mobileEmblaApi: any) => {
+    setMobileSelectedIndex(mobileEmblaApi.selectedScrollSnap());
+  }, []);
 
   useEffect(() => {
     if (!emblaApi) return;
+    if (!mobileEmblaApi) return;
 
     onSelect(emblaApi);
+    onSelectMobile(mobileEmblaApi);
 
     emblaApi.on("reInit", onSelect).on("select", onSelect);
-  }, [emblaApi, onSelect]);
+    mobileEmblaApi.on("reInit", onSelectMobile).on("select", onSelectMobile);
+  }, [emblaApi, onSelect, mobileEmblaApi, onSelectMobile]);
 
   return (
     <section className={"px-[10.938vw] mt-[10.417vw] flex flex-col"}>
       <div
         className={
-          "flex flex-row w-full justify-between items-center z-[1] -mb-[2.604vw]"
+          "flex flex-row w-full justify-between items-center z-[1] mb-[3.529vw] lg:!-mb-[2.604vw]"
         }
       >
         <span
-          className={"font-kavaimo text-[5.208vw] text-yellow leading-[80%]"}
+          className={
+            "font-kavaimo text-[13.647vw] lg:!text-[5.208vw] text-yellow leading-[80%]"
+          }
         >
           Games
         </span>
-        <ArrowButton link={"#"} text={"Go to App"} />
+        <ArrowButton
+          link={"#"}
+          text={"Go to App"}
+          className={"hidden lg:!flex"}
+        />
       </div>
-      <div className={"overflow-hidden w-full"} ref={emblaRef}>
-        <div className={"flex flex-row w-full"}>
+      <div className={"overflow-hidden w-full hidden lg:!block"} ref={emblaRef}>
+        <div className={"flex-row w-full flex"}>
           {slides.map((slide, index) => (
             <Slide
               key={index}
@@ -303,12 +333,28 @@ export default function Games() {
           ))}
         </div>
       </div>
+      <div className={"overflow-hidden w-full lg:!hidden"} ref={mobileEmblaRef}>
+        <div className={"flex flex-row w-full"}>
+          {slides.map((slide, index) => (
+            <Slide
+              key={index}
+              tags={slide.tags}
+              network={slide.network}
+              image={slide.image}
+              gameName={slide.gameName}
+              description={slide.description}
+              link={slide.link}
+              type={index == mobileSelectedIndex ? "currentSlide" : "nextSlide"}
+            />
+          ))}
+        </div>
+      </div>
       <div
         className={
-          "w-full flex flex-col items-center justify-center mt-[1.563vw]"
+          "w-full flex flex-col items-center justify-center mt-[3.529vw] lg:!mt-[1.563vw]"
         }
       >
-        <div className={"flex flex-row gap-[0.182vw]"}>
+        <div className={"hidden lg:!flex flex-row gap-[0.182vw]"}>
           {slides.map((_, index) => (
             <motion.div
               key={index}
@@ -321,6 +367,33 @@ export default function Games() {
             />
           ))}
         </div>
+        <div className={"flex lg:!hidden flex-row gap-[0.847vw]"}>
+          {slides.map((_, index) => (
+            <motion.div
+              key={index}
+              className={"h-[1.412vw] rounded-full border-white border"}
+              animate={
+                index == mobileSelectedIndex
+                  ? { backgroundColor: "#fff", width: "4.235vw" }
+                  : { width: "1.412vw" }
+              }
+            />
+          ))}
+        </div>
+        <Link
+          href={"#"}
+          className={
+            "mt-[4.706vw] lg:!hidden flex flex-col justify-center items-center py-[2.353vw] w-full bg-yellow rounded-[2.353vw]"
+          }
+        >
+          <span
+            className={
+              "text-gray-light text-[3.765vw] font-medium leading-[110%] font-helvetica-now"
+            }
+          >
+            Go to game store
+          </span>
+        </Link>
       </div>
     </section>
   );
